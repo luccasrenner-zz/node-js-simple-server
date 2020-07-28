8cfb449a3f0e6d079cb4c1a856978904214458f9
Lucas Campos de Souza <lucas.camposdesouza@mercadolibre.com>
2020-07-28 19:55:22 -0300

    resolve files

--- index.js
+++ index.js
@@ -3,18 +3,10 @@ const jwt = require('jsonwebtoken');
 
 const CONSTANTS = require('./constants');
 
-const informacion = { nombre : "Lucas" };
-const firma = "mi_password";
-const token = jwt.sign(informacion, firma);
-
-console.log(token);
-
 const app = express();
 
-app.post('/', (req, res) => {
-    const {
-        nombre
-    } = req.body
+app.get('/', (req, res) => {
+    
 })
 
 
